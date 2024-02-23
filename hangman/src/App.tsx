a0b81f0abba3f280e272e04a28b435868b6a7b2a
hadayahh <habaza.dev@gmail.com>
2024-02-22 19:43:23 -0500

    initial component structure

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,10 +1,30 @@
-function App() {
-  
+import { useState } from 'react'
+import words from "./wordList.json"
 
+import {HangmanDrawing} from "./Hangmandrawing"
+import { HangmanWord } from './HangmanWord'
+import { Keyboard } from './Keyboard'
+
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  
+  //explicity defining a type for useState
+  //Ex: useState<string>('') or useState<number>(0)
+  //In this Example: useState <string[]>([])
+  //An array of strings
+  const [guessedLetter, setGuessedLetter] = useState <string[]>([])
+  console.log(wordToGuess)
+  
   return (
-    <>
-    <h1>hello</h1>
-    </>
+    <div style={{maxWidth: '800px', display: 'flex', flexDirection: 'column', gap: '2rem', margin: 'auto', alignItems: 'center'}}>
+      <div style={{fontSize: '2rem', textAlign: 'center'}}>Lose Win</div>
+      <HangmanDrawing/>
+      <HangmanWord/>
+      <Keyboard/>
+    </div>
   )
 }
 
