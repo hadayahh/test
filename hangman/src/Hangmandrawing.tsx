04ec7428d9a00cd75f746e5b98ead304b09a3212
hadayahh <habaza.dev@gmail.com>
2024-02-24 18:05:17 -0500

    hangman drawing css

--- hangman/src/Hangmandrawing.tsx
+++ hangman/src/Hangmandrawing.tsx
@@ -1,3 +1,10 @@
 export function HangmanDrawing(): any{
-
+    return (
+    <div>
+        <div style={{height: "50px", width: "10px", background: "black", position: "absolute", top: "0", right: "0"}}></div>
+        <div style={{height: "10px", width: "200px", background: "black", marginLeft: "120px"}}></div>
+        <div style={{height: "400px", width: "10px", marginLeft: "120px", background: "black"}}></div>
+        <div style={{height: "10px", width: "270px", background: "black"}}></div>
+    </div>
+    )
 }
