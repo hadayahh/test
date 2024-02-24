940f311191855a2313a15a0dd96e9f0ea694640e
hadayahh <habaza.dev@gmail.com>
2024-02-24 18:51:42 -0500

    drawing legs

--- hangman/src/Hangmandrawing.tsx
+++ hangman/src/Hangmandrawing.tsx
@@ -3,7 +3,7 @@ const HEAD = (
 )
 
 const BODY = (
-    <div style={{background: "black", height: "100px", width: "10px", position: "absolute", top: "115px", right: "0px"}}></div>
+    <div style={{background: "black", height: "100px", width: "10px", position: "absolute", top: "120px", right: "0px"}}></div>
 )
 
 const RIGHT_ARM = (
@@ -14,6 +14,14 @@ const LEFT_ARM = (
     <div style={{background: "black", height: "10px", width: "100px", position: "absolute", top: "150px", right: "10px", rotate:"30deg", transformOrigin: "right bottom"}}></div>
 )
 
+const RIGHT_LEG = (
+    <div style={{background: "black", height: "10px", width: "100px", position: "absolute", top: "210px", right: "-90px", rotate:"60deg", transformOrigin: "left bottom"}}></div>
+)
+
+const LEFT_LEG = (
+    <div style={{background: "black", height: "10px", width: "100px", position: "absolute", top: "210px", right: 0, rotate:"-60deg", transformOrigin: "right bottom"}}></div>
+)
+
 export function HangmanDrawing(): any{
     return (
     <div style={{position: "relative"}}>
@@ -21,6 +29,8 @@ export function HangmanDrawing(): any{
         {BODY}
         {RIGHT_ARM}
         {LEFT_ARM}
+        {RIGHT_LEG}
+        {LEFT_LEG}
         <div style={{height: "50px", width: "10px", background: "black", position: "absolute", top: "0", right: "0"}}></div>
         <div style={{height: "10px", width: "200px", background: "black", marginLeft: "120px"}}></div>
         <div style={{height: "400px", width: "10px", marginLeft: "120px", background: "black"}}></div>
