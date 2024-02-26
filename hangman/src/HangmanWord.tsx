cf52c54f5c05d2a0e8e58d009019829f98d44222
hadayahh <habaza.dev@gmail.com>
2024-02-25 20:16:54 -0500

    hangman word guess test

--- hangman/src/HangmanWord.tsx
+++ hangman/src/HangmanWord.tsx
@@ -1,3 +1,10 @@
 export function HangmanWord(): any {
-    
+    const word = "hadi"
+    const guessedLetters= ["h"]
+
+    return (
+        <div style={{display: "flex", gap: ".25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace"}}>{word.split(" ").map((letter, index) => (
+           <span>{letter}</span>
+        ))}</div>
+    )
 }
