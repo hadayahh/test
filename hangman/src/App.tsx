1120ccb02a03f6da58f1b5590a751dac47d130d8
hadayahh <habaza.dev@gmail.com>
2024-03-22 17:18:00 -0400

    final touches

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -7,6 +7,18 @@ import { Keyboard } from './Keyboard'
 
 import './App.css';
 
+import swal from 'sweetalert';
+
+
+
+function share() {
+  let url = document.location.href;
+  const cb = navigator.clipboard;
+  cb.writeText(url).then(() => {
+    swal('URL has been copied!')
+  })
+
+}
 function getWord(){
   return words[Math.floor(Math.random() * words.length)]
 }
@@ -82,19 +94,20 @@ function App() {
 
   return (
     
-    <div style={{maxWidth: '800px', display: 'flex', flexDirection: 'column', gap: '2rem', margin: 'auto', alignItems: 'center'}}>
-      <h1>Things you should know</h1>
+    <div style={{maxWidth: '1100px', display: 'flex', flexDirection: 'column', gap: '2rem', margin: 'auto', alignItems: 'center'}}>
+      <h1 style={{fontSize: "3rem"}}>Things you should consider</h1>
+      <ul style={{fontSize: '25px'}}>
+        <li>The objective of hangman is to guess the secret word before the stick figure is hung.</li>
+        <li>Players take turns selecting letters to narrow the word down, you may choose to play solo as well.</li>
+        <li>Gameplay continues until the players guess the word or they run out of guesses and the stick figure is hung.</li>
+        <li>Feel free to refresh the browser should you want to tackle a shorter word in length.</li>
+      </ul>
+      {/* <h1>Rules to consider</h1>
       <ul style={{fontSize: '25px'}}>
         <li>The object of hangman is to guess the secret word before the stick figure is hung. Players take turns selecting letters to narrow the word down.</li>
         <li>Players can take turns or work together. Gameplay continues until the players guess the word or they run out of guesses and the stick figure is hung.</li>
         <li>If you want to play with younger kids, use a snowman instead of a hangman to avoid scaring or offending anyone.</li>
-      </ul>
-      <h1>Rules to consider</h1>
-      <ul style={{fontSize: '25px'}}>
-        <li>The object of hangman is to guess the secret word before the stick figure is hung. Players take turns selecting letters to narrow the word down.</li>
-        <li>Players can take turns or work together. Gameplay continues until the players guess the word or they run out of guesses and the stick figure is hung.</li>
-        <li>If you want to play with younger kids, use a snowman instead of a hangman to avoid scaring or offending anyone.</li>
-      </ul>
+      </ul> */}
       <h3 style={{fontSize: "3rem"}}>Have at it!</h3>
       <div style={{fontSize: '2rem', textAlign: 'center'}}>{isWinner && 'Winner! - Refresh to try again.'} {isLoser && 'Nice Try! - Refresh to try again.'}</div>
       <HangmanDrawing numberOfGuesses={incorrectLetters.length}/>
@@ -102,6 +115,7 @@ function App() {
       <div style={{alignSelf: "stretch"}}>
         <Keyboard disabled={isWinner || isLoser} activeLetters={guessedLetters.filter(letter => wordToGuess.includes(letter))} inactiveLetters={incorrectLetters} addGuessedLetter={addGuessedLetter}/>
       </div>
+      <button id= 'button' onClick={() => share()}>Share with friends!</button>
     </div>
   )
 }
